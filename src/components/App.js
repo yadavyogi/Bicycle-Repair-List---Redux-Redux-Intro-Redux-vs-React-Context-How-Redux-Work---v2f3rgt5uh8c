9e554f65ff46ca9a1c5f0bb26de6e5ab447e94bd
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-01-06 21:54:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+/*import React, {Component, useState} from "react";
+import '../styles/App.css'; */
 
-const App = () => {
+import React from "react";
+import "../styles/App.css";
+import Form from "./Form";
+import List from "./List";
+
+function App() {
   return (
-    <div id="main"></div>
-  )
+    <div className="App">
+      <header>Bicycle Repair App</header>
+      <Form />
+      <hr></hr>
+      <List />
+    </div>
+  );
 }
-
 
 export default App;
